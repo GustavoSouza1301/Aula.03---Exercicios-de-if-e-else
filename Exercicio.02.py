7596ba03242c130593abe8d20004d7859ea09d70
Gustavo Souza <gggutavo1301@gmail.com>
2025-04-01 09:36:39 -0300

    Exercicio 2, pratica  de operações

---
+++ Exercicio.02.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+soma = num1 + num2
+subtracacao = num1 - num2
+divisao = num1 / num2
+multiplicacao = num1 * num2
+
+print(f"Resultado soma: {soma}\n"
+f"Resultado subtração: {subtracacao}\n"
+f"Resultado divisão: {divisao}\n"
+f"Resultado multiplicação:{multiplicacao} ")
