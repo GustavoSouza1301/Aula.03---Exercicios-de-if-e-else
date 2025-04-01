1d9ad53f5bb465ebb572f3ec5e6bd0b5c6f9f37a
Gustavo Souza <gggutavo1301@gmail.com>
2025-04-01 10:57:54 -0300

    Criação de um sistema de media e a provação, usando if e else.

---
+++ Exercicio.05.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Primeira nota"))
+nota2 = float(input("Segunda nota"))
+nota3 = float(input("Terceira nota"))
+mediaNota = (nota1 + nota2 + nota3 )/3
+
+if mediaNota >= 7:
+    print(f"Média: {mediaNota:.2f}, Aprovado!")
+
+else:
+    print(f"Média: {mediaNota:.2f}, Reprovado!")
