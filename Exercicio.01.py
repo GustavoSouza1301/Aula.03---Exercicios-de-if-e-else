62390456e1c2a1c71cf5cbb0833d3babbbfdd140
Gustavo Souza <gggutavo1301@gmail.com>
2025-04-01 09:18:12 -0300

    Atualização do exercicio

--- Exercicio.01.py
+++ Exercicio.01.py
@@ -1,5 +1,9 @@
-nome_usuario = input("Digite o seu nome: ")
-idade_usuario = int(input("Digite a sua idade: "))
-salario_usuario = float(input("Digite o seu salario: "))
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+salario = float(input("Digite o seu salario: "))
+aumento = int(input("Qual o percentual de aumento: "))
+valorReal = salario * aumento/100
+novoSalario = salario + valorReal
 
-print(f"Olá{nome_usuario}\nsua idade é: {idade_usuario}\n e o seu salario é: R${salario_usuario}")
+print(f"Olá {nome}, sua idade é: {idade}\n e o seu salario é: R${salario:.2f} e voce recebeu\n"
+      f"1{valorReal:.2f} de aumento e seu novo salario é R${novoSalario:.2f}")
