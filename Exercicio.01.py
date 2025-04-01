4a952d27bf06e21cbc216ea3f159c0294abfea5b
Gustavo Souza <gggutavo1301@gmail.com>
2025-04-01 08:53:40 -0300

    Primeiro exercicio do dia, comandos simple

---
+++ Exercicio.01.py
@@ -0,0 +1,5 @@
+nome_usuario = input("Digite o seu nome: ")
+idade_usuario = int(input("Digite a sua idade: "))
+salario_usuario = float(input("Digite o seu salario: "))
+
+print(f"Olá{nome_usuario}\nsua idade é: {idade_usuario}\n e o seu salario é: R${salario_usuario}")
